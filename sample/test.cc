541e7b4d6e3026889f01c123e7be4aab98ebb83f
iikare <13582030+iikare@users.noreply.github.com>
2023-05-11 19:31:20 -0700

    empty line macro

--- sample/test.cc
+++ sample/test.cc
@@ -5,10 +5,17 @@ int main(int argc, char* argv[]) {
   logQ("this is a demonstration of this logging library");
   
   logW(LL_WARN, "set debug levels");
+  
+  // empty line
+  logE();
 
   logC("compare", "two values");
 
+  // empty line
+  logE();
+
   logQ("an", "example", "of", "the", "power", "of", "variadic", "logging", 1234);
+  
 
   return 0;
 }
